48f080d3ab4251710673e0c4ab456f576e46eeae
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-12-08 19:12:04 +0100

    Error handling

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,4 +1,26 @@
+use axum::{http::StatusCode, response::IntoResponse};
+
 pub mod day_01;
 pub mod day_04;
 pub mod day_06;
 pub mod day_07;
+pub mod day_08;
+
+#[derive(Debug)]
+pub struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+  fn into_response(self) -> axum::response::Response {
+    (
+      StatusCode::INTERNAL_SERVER_ERROR,
+      format!("Something went wrong: {}", self.0),
+    )
+      .into_response()
+  }
+}
+
+impl<E: Into<anyhow::Error>> From<E> for AppError {
+  fn from(err: E) -> Self {
+    Self(err.into())
+  }
+}
