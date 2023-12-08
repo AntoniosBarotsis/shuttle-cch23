d63255c28983557e4a4971215e528c765fa2c355
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-12-08 19:12:06 +0100

    day 8

---
+++ src/days/day_08.rs
@@ -0,0 +1,47 @@
+use anyhow::anyhow;
+use axum::{extract::Path, routing::get, Router};
+use serde_json::Value;
+
+use super::AppError;
+
+pub fn get_routes() -> Router {
+  Router::new()
+    .route("/8/weight/:id", get(task_1))
+    .route("/8/drop/:id", get(task_2))
+}
+
+async fn get_weight(id: u32) -> Result<u64, AppError> {
+  let res = ureq::get(&format!("https://pokeapi.co/api/v2/pokemon/{id}"))
+    .call()?
+    .into_string()?;
+
+  let root = serde_json::from_str::<Value>(&res)?;
+
+  let weight = root
+    .get("weight")
+    .ok_or_else(|| anyhow!("Weight not found"))?
+    .as_number()
+    .and_then(serde_json::Number::as_u64)
+    .ok_or_else(|| anyhow!("Weight not a number"))?;
+
+  Ok(weight)
+}
+
+async fn task_1(Path(id): Path<u32>) -> Result<String, AppError> {
+  let weight = get_weight(id).await?;
+
+  let weight_kg = weight / 10;
+
+  Ok(weight_kg.to_string())
+}
+
+async fn task_2(Path(id): Path<u32>) -> Result<String, AppError> {
+  let weight = get_weight(id).await?;
+
+  let weight_kg = weight / 10;
+
+  #[allow(clippy::cast_precision_loss)]
+  let res = (2.0f64 * 9.825 * 10.0).sqrt() * (weight_kg as f64);
+
+  Ok(res.to_string())
+}
